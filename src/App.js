83d3c92c48339c00d077f47de06cdb5510a877de
John Vasile <johny_tnt@yahoo.com>
2021-02-09 22:25:43 -0500

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import contacts from './contacts.json';
+// import MovieList from './components/MovieList';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App" >
-      </div>
-    );
-  }
+
+function App(){
+
+    const fiveMovies = contacts.slice(0,5)
+    console.log(fiveMovies)
+    <table>
+    <tr>
+      <th>Picture</th>
+      <th>Name</th> 
+      <th>Popularity</th>
+      <th>Action</th>
+    </tr>
+    const showFIve = () =>{
+      return fiveMovies.map(eachContact => {
+        return (<tr>
+          <td><img src={eachContact.pictureUrl}/></td>
+          <td>{eachContact.name}</td>
+          <td>{eachContact.popularity}</td>
+        </tr>)
+      })
+    }
+
+  return(
+    <div>
+      Hello
+    </div>
+  )
 }
 
+
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class App extends React.Component {
+//   render() {
+
+//     return (
+//       <div className="App" >
+//       console.log(contacts)
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
